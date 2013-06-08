5a599492ec609c162be070b0310fe62f5cd611ff
Otavio Fernandes <otavio@neton.com.br>
2013-06-08 01:11:10 -0300

    Created the Finder service Provider

---
+++ src/SilexFinder/FinderServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Silex Finder Service provider.
+ * @author Otávio Fernandes <otavio@netonsolucoes.com.br>
+ */
+
+namespace SilexFinder;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Finder\Finder;
+
+class FinderServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * Register and configure the service provider.
+     *
+     * @param Application $app
+     */
+    public function register(Application $app)
+    {
+        // finder factory configuration
+        $app['finder'] = $app->share(function() use ($app){
+            return new Finder();
+        });
+
+    }
+
+    /**
+     * Bootstrap the application.
+     *
+     * @param Application $app
+     */
+    public function boot(Application $app)
+    {
+
+    }
+}
